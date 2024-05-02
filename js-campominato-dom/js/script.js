a205775c9c769169eb3bbd9b8b7eb151002b81e1
lucianocretella95 <159431348+lucianocretella95@users.noreply.github.com>
2024-05-02 17:37:47 +0200

    Add files via upload

--- js-campominato-dom/js/script.js
+++ js-campominato-dom/js/script.js
@@ -3,22 +3,20 @@ let easy = document.getElementById("ez");
 let mid = document.getElementById("mid");
 let hard = document.getElementById("hard");
 
+easy.addEventListener("click", function () {
+  for (let i = 1; i <= 100; );
+});
+
 for (let i = 1; i <= 100; i++) {
   const element = `<div class="square-easy ${i}">${i}</div>`;
   griglia.innerHTML += element;
 }
 
-easy.addEventListener("click", function () {
-  griglia.classList.remove("square-mid");
-  griglia.classList.remove("square-hard");
-  console.log("click");
-});
-
-for (let i = 1; i <= 80; i++) {
-  const element = `<div class="square-mid ${i}">${i}</div>`;
-  griglia.innerHTML += element;
-}
-for (let i = 1; i <= 50; i++) {
-  const element = `<div class="square-hard ${i}">${i}</div>`;
-  griglia.innerHTML += element;
-}
+// for (let i = 1; i <= 80; i++) {
+//   const element = `<div class="square-mid ${i}">${i}</div>`;
+//   griglia.innerHTML += element;
+// }
+// for (let i = 1; i <= 50; i++) {
+//   const element = `<div class="square-hard ${i}">${i}</div>`;
+//   griglia.innerHTML += element;
+// }
