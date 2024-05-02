1e5d5d8bccb744826aa8c4dcb6bb3f85fa7675f9
lucianocretella95 <159431348+lucianocretella95@users.noreply.github.com>
2024-05-02 18:00:38 +0200

    Add files via upload

--- js-campominato-dom/js/script.js
+++ js-campominato-dom/js/script.js
@@ -4,17 +4,25 @@ let mid = document.getElementById("mid");
 let hard = document.getElementById("hard");
 
 easy.addEventListener("click", function () {
+  griglia.innerHTML = "";
   for (let i = 1; i <= 100; i++) {
     const element = `<div class="square-easy ${i}">${i}</div>`;
     griglia.innerHTML += element;
   }
 });
 
-// for (let i = 1; i <= 80; i++) {
-//   const element = `<div class="square-mid ${i}">${i}</div>`;
-//   griglia.innerHTML += element;
-// }
-// for (let i = 1; i <= 50; i++) {
-//   const element = `<div class="square-hard ${i}">${i}</div>`;
-//   griglia.innerHTML += element;
-// }
+mid.addEventListener("click", function () {
+  griglia.innerHTML = "";
+  for (let i = 1; i <= 80; i++) {
+    const element = `<div class="square-mid ${i}">${i}</div>`;
+    griglia.innerHTML += element;
+  }
+});
+
+hard.addEventListener("click", function () {
+  griglia.innerHTML = "";
+  for (let i = 1; i <= 50; i++) {
+    const element = `<div class="square-hard ${i}">${i}</div>`;
+    griglia.innerHTML += element;
+  }
+});
